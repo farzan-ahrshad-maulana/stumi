7454e2ef6a98b6ccb64253b79e2d06f51a44b1ed
farzan-ahrshad-maulana <fzan.ahrshad@gmail.com>
2026-06-04 05:02:20 +0700

    Register Router.

--- app/main.py
+++ app/main.py
@@ -1,18 +1,16 @@
 from fastapi import FastAPI
 
-app = FastAPI(
-    title="Stumi",
-    version="0.1.0"
-)
+from app.api.journals import router as journal_router
+
+app = FastAPI(title="Stumi", version="0.1.0")
+app.include_router(journal_router)
+
 
 @app.get("/")
 def root():
-    return {
-        "message": "Welcome to Stumi"
-    }
+    return {"message": "Welcome to Stumi"}
+
 
 @app.get("/health")
 def health():
-    return {
-        "status": "healthy"
-    }
+    return {"status": "healthy"}
