6b0732fd2d2f1f6d3a24d3f2163f1be0078332f4
farzan-ahrshad-maulana <fzan.ahrshad@gmail.com>
2026-06-04 12:21:32 +0700

    Add Response Schema.

--- app/schemas/journal.py
+++ app/schemas/journal.py
@@ -1,5 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class JournalCreate(BaseModel):
     pdf_url: str
+
+
+class JournalResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+
+    title: str
