ec1724f1305836f2fd727d6eabadc104cb50cb72
farzan-ahrshad-maulana <fzan.ahrshad@gmail.com>
2026-06-05 15:53:36 +0700

    Add ask_question func in rag service.

--- app/services/rag_service.py
+++ app/services/rag_service.py
@@ -6,3 +6,24 @@ from app.services.llm_service import (
 from app.services.retrieval_service import (
     retrieve_chunks,
 )
+
+
+def ask_question(
+    db: Session,
+    question: str,
+):
+
+    results = retrieve_chunks(
+        db=db,
+        question=question,
+        limit=5,
+    )
+
+    context = "\n\n".join(row.chunk_text for row in results)
+
+    answer = generate_answer(
+        question=question,
+        context=context,
+    )
+
+    return answer
